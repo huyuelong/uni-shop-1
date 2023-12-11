62ce371218810c5d20fae276a498498a19055fd7
huyuelong <949910047@qq.com>
2023-12-11 22:38:02 +0800

    完成了商品详情页面的开发

--- unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
@@ -2,11 +2,101 @@
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
-    return {};
+    return {
+      // 商品详情对象
+      goods_info: {},
+      // 左侧按钮组的配置对象
+      options: [{
+        icon: "shop",
+        text: "店铺"
+      }, {
+        icon: "cart",
+        text: "购物车",
+        info: 2
+      }],
+      // 右侧按钮组的配置对象
+      buttonGroup: [
+        {
+          text: "加入购物车",
+          backgroundColor: "#ff0000",
+          color: "#fff"
+        },
+        {
+          text: "立即购买",
+          backgroundColor: "#ffa200",
+          color: "#fff"
+        }
+      ]
+    };
+  },
+  onLoad(options) {
+    const goods_id = options.goods_id;
+    this.getGoodsDetail(goods_id);
+  },
+  methods: {
+    async getGoodsDetail(goods_id) {
+      const { data: res } = await common_vendor.index.$http.get("/api/public/v1/goods/detail", { goods_id });
+      if (res.meta.status !== 200)
+        return common_vendor.index.$showMsg();
+      res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;"').replace(/webp/g, "jpg");
+      this.goods_info = res.message;
+    },
+    // 实现轮播图的预览效果
+    preview(i) {
+      common_vendor.index.previewImage({
+        // 预览时，默认显示图片的索引
+        current: i,
+        // 所有图片 url 地址的数组
+        urls: this.goods_info.pics.map((x) => x.pics_big)
+      });
+    },
+    // 左侧按钮的点击事件处理函数
+    onClick(e) {
+      if (e.content.text === "购物车") {
+        common_vendor.index.switchTab({
+          url: "/pages/cart/cart"
+        });
+      }
+    }
   }
 };
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_goods_nav2 = common_vendor.resolveComponent("uni-goods-nav");
+  (_easycom_uni_icons2 + _easycom_uni_goods_nav2)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_goods_nav = () => "../../uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.js";
+if (!Math) {
+  (_easycom_uni_icons + _easycom_uni_goods_nav)();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return common_vendor.e({
+    a: $data.goods_info.goods_name
+  }, $data.goods_info.goods_name ? {
+    b: common_vendor.f($data.goods_info.pics, (item, i, i0) => {
+      return {
+        a: item.pics_big,
+        b: common_vendor.o(($event) => $options.preview(i), i),
+        c: i
+      };
+    }),
+    c: common_vendor.t($data.goods_info.goods_price),
+    d: common_vendor.t($data.goods_info.goods_name),
+    e: common_vendor.p({
+      type: "star",
+      size: "18",
+      color: "gray"
+    }),
+    f: $data.goods_info.goods_introduce,
+    g: common_vendor.o($options.onClick),
+    h: common_vendor.o(_ctx.buttonClick),
+    i: common_vendor.p({
+      fill: true,
+      options: $data.options,
+      buttonGroup: $data.buttonGroup
+    })
+  } : {});
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/94991/Documents/HBuilderProjects/uni-shop-1/subpkg/goods_detail/goods_detail.vue"]]);
 wx.createPage(MiniProgramPage);
